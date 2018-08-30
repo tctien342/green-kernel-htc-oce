4f9baf60ac956e32b98721f91041d9c93275a594
SaintNo <tctien342@gmail.com>
2018-08-31 01:13:18 +0700

    drivers: Sysfs interface to Enable/Disable CRC

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -67,8 +67,13 @@ static struct workqueue_struct *workqueue;
 struct workqueue_struct *stats_workqueue = NULL;
 static const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
 
-bool use_spi_crc = 1;
-module_param(use_spi_crc, bool, 0);
+/*
+ * Enabling software CRCs on the data blocks can be a significant (30%)
+ * performance cost, and for other reasons may not always be desired.
+ * So we allow it it to be disabled.
+ */
+bool use_spi_crc = 0;
+module_param(use_spi_crc, bool, 0644);
 
 static int stats_interval = MMC_STATS_INTERVAL;
 #define K(x) ((x) << (PAGE_SHIFT - 10))
